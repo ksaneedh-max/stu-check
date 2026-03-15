6f635ccee2f41e32301b3d4460a0028240c2bd76
ksaneedh-max <ksaneedh@gmail.com>
2026-03-15 17:10:12 +0530

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -2,5 +2,6 @@ require("dotenv").config();
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  PORTAL_URL: "https://academia.srmist.edu.in"
+  PORTAL_URL:
+    process.env.PORTAL_URL || "https://academia.srmist.edu.in"
 };
